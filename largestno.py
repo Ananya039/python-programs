3431e32b2da997f9bccec0ac1f8f4f1fd324e571
Ananya039 <ananya98561397@gmail.com>
2025-05-27 13:07:15 +0530

    Initial commit

---
+++ largestno.py
@@ -0,0 +1,9 @@
+a = int(input("Enter num 1: "))
+b = int (input("Enter num 2: "))
+c = int(input("Enter num 3: "))
+if (a>b) and (a>c):
+    print("Largest no. is: ", a)
+elif(b>a) and (b>c):
+    print("Largest no. is: ",b)
+else:
+    print ("Largest no. is: ", c)    
