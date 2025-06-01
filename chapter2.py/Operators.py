b5c9a17d9965faf31f035953e65f184ea79038a8
Ananya039 <ananya98561397@gmail.com>
2025-06-01 06:43:42 +0530

    Python Program added

---
+++ chapter2.py/Operators.py
@@ -0,0 +1,27 @@
+# Arithmetic Operators
+
+a = 10
+b = 5
+c = a + b
+print("Addition:", c)
+
+#Assignment Operators
+a =  4-2   #  Assign 4-2 in a
+print(a)
+b = 6 
+b += 2 # Increment the value of  b by 2 and assign it to b
+print(b) 
+
+# Comparison Operators
+d= 5<4
+print(d)
+
+# Logical Operators
+e = True or False
+print(e)
+
+#Truth Table of 'and'
+print("True and False is ", True and False)
+print("True and True is ", True and True)
+print("False and True is ", False and True)
+print("False and False is ", False and False)
