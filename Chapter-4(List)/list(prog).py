1f459f8b17def0d83dc600fc4befac2b0bf9dafb
Ananya039 <ananya98561397@gmail.com>
2025-06-03 13:26:10 +0530

    Python Program added

---
+++ Chapter-4(List)/list(prog).py
@@ -0,0 +1,7 @@
+friend = ["annya", 4,True, 3.14, "hello", 5, "world", 6, "python", ]
+
+print(friend[4])
+friend[4] = "hi"
+print(friend[4])
+
+print(friend[1:3])  # Slicing the first three elements
